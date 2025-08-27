d0409cf43949d56ea4b1eddb0ebd9dd3e1582273
jizca7 <jizca7@gmail.com>
2025-08-27 13:44:53 +0200

    Update index.js

---
+++ ejercicio1/index.js
@@ -0,0 +1,17 @@
+//1.1
+const boton = document.querySelector('.showme');
+console.log(boton);
+//1.2
+const h1 = document.querySelector('#pillado');
+console.log(h1);
+//1.3
+const parrafos = document.querySelectorAll('p');
+console.log(parrafos);
+//1.4
+const pokemons = document.querySelectorAll('.pokemon');
+console.log(pokemons);
+//1.5
+const testMeElems = document.querySelectorAll('[data-function="testMe"]');
+console.log(testMeElems);
+//1.6
+console.log(testMeElems[2]);
